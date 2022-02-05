e2b1b34b2ed75772582715169ece0d968dc0b3fc
Dmitry Pimenov <dmitryspimenov@gmail.com>
2022-02-05 13:24:58 -0800

    Initial Commit

---
+++ Mwe/MweApp.swift
@@ -0,0 +1,17 @@
+//
+//  MweApp.swift
+//  Mwe
+//
+//  Created by Dmitry Pimenov on 2/5/22.
+//
+
+import SwiftUI
+
+@main
+struct MweApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
